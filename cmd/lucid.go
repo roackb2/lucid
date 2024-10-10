06528dd3341264b8a448539c0af394f64e18f357
Jay/Fienna Liang <roackb2@gmail.com>
2024-10-10 15:02:17 +0800

    Refactor foundation models

--- cmd/lucid.go
+++ cmd/lucid.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/openai/openai-go"
-	"github.com/openai/openai-go/option"
-	"github.com/spf13/viper"
+	"github.com/roackb2/lucid/internal/foundation_models"
 )
-
-var (
-	vp            *viper.Viper
-	openai_client *openai.Client
-)
-
-func init() {
-	vp := viper.New()
-	vp.SetEnvPrefix("lucid")
-	vp.AutomaticEnv()
-	vp.SetConfigName("dev")
-	vp.AddConfigPath("configs/")
-	vp.SetConfigType("yaml")
-	err := vp.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("fatal error config file: %s", err))
-	}
-	openai_api_key := vp.GetString("openai.api_key")
-	if openai_api_key == "" {
-		panic("openai.api_key is not set")
-	}
-	openai_client = openai.NewClient(
-		option.WithAPIKey(openai_api_key),
-	)
-}
 
 func main() {
-	chatCompletion, err := openai_client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
-		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.UserMessage("Say this is a test"),
-		}),
-		Model: openai.F(openai.ChatModelGPT4o),
-	})
+	response, err := foundation_models.Chat("Say this is a test")
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(chatCompletion.Choices[0].Message.Content)
+	fmt.Println(response)
 }
---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var Config Configuration
+
+type Configuration struct {
+	OpenAI struct {
+		APIKey string `mapstructure:"api_key"`
+	} `mapstructure:"openai"`
+}
+
+func init() {
+	viper.SetConfigName("dev")    // name of config file (without extension)
+	viper.SetConfigType("yaml")   // required if config file doesn't have an extension
+	viper.AddConfigPath("config") // look for config in the working directory
+
+	viper.AutomaticEnv() // override config file with environment variables
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		log.Fatalf("Unable to decode into struct: %s", err)
+	}
+
+	log.Println("Configuration loaded successfully")
+}
