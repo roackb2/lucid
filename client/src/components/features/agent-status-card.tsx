7145730cbaeeeed082e95c4eac080a6bb27d647c
Jay/Fienna Liang <roackb2@gmail.com>
2024-11-10 12:38:23 +0800

    Fix agent status display & add timestamp display

--- client/src/components/features/agent-status-card.tsx
+++ client/src/components/features/agent-status-card.tsx
@@ -2,7 +2,7 @@
 import { definitions } from '@/types/apiTypes'
 import { AgentNotificationTypes } from '@/types/layout'
 import { useEffect, useState } from 'react'
-
+import dayjs from 'dayjs'
 interface AgentStatusCardProps {
   agentId: string
   messages: AgentNotificationTypes[]
@@ -11,13 +11,14 @@ interface AgentStatusCardProps {
 
 export default function AgentStatusCard({ agentId, messages }: AgentStatusCardProps) {
   const lastMessage = messages[messages.length - 1]
-  const lastStatusMessage: definitions['worker.WorkerStatusNotification'] | undefined = messages.find(
+  const lastTimestamp = dayjs(lastMessage?.timestamp).format('YYYY-MM-DD HH:mm:ss')
+  const lastStatusMessage: definitions['worker.WorkerStatusNotification'] | undefined = messages.findLast(
     (message) => 'status' in message,
   )
-  const lastProgressMessage: definitions['worker.WorkerProgressNotification'] | undefined = messages.find(
+  const lastProgressMessage: definitions['worker.WorkerProgressNotification'] | undefined = messages.findLast(
     (message) => 'progress' in message,
   )
-  const lastResponseMessage: definitions['worker.WorkerResponseNotification'] | undefined = messages.find(
+  const lastResponseMessage: definitions['worker.WorkerResponseNotification'] | undefined = messages.findLast(
     (message) => 'response' in message,
   )
 
@@ -26,6 +27,7 @@ export default function AgentStatusCard({ agentId, messages }: AgentStatusCardPr
       <p className="text-sm font-medium text-gray-500">
         {agentId} ({lastStatusMessage?.status})
       </p>
+      <p className="text-xs text-gray-500">{lastTimestamp}</p>
       {lastProgressMessage?.progress && (
         <p className="text-sm">{lastProgressMessage.progress}</p>
       )}
